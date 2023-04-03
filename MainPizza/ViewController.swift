79866508a44d16de4b3a4c8806541d43d9d74837
Artur Zainulov <artur.zainulov@gmail.com>
2023-04-03 11:19:37 +0600

    Initial Commit

---
+++ MainPizza/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MainPizza
+//
+//  Created by Элина Вишневская🍒 on 03.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
